e2c5b1e2b41d776cb200144971ee88ab516e81db
mufaddalvirpur <mufaddalvirpuri04@gmail.com>
2026-01-05 16:06:44 +0530

    deployment fix

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const MONGO_URI = process.env.MONGO_URI;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", process.env.VF_URL],
     credentials: true,
   })
 );
